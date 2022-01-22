8ad4af2c66d78907de6956d2df5b0d26ead9d10b
Shreyas <schicker@purdue.edu>
2022-01-22 15:55:37 -0500

    vscode/

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, url_for,redirect
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
+    
     return render_template('index.html')
 
 @app.route('/signup')
