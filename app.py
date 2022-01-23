817a67350d4e8d754fb71cf16cb7c9dd1aed5975
Shreyas <schicker@purdue.edu>
2022-01-23 03:10:20 -0500

    creating all new instances of tables for the db

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
 db = SQLAlchemy(app)
-
+db.create_all()
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +17,7 @@ class Users(db.Model):
     password =  db.Column(db.String(200), nullable=False)
     confirm = db.Column(db.String(200), nullable=False)
     boilerBucks = db.Column(db.Integer, primary_key = False)
+    db.session.commit()
     def __repr__(self):
         return self.id
 
@@ -27,6 +28,7 @@ class Bets(db.Model):
     betAmount = db.Column(db.Integer, primary_key=False)
     payout =  db.Column(db.Integer, primary_key=False)
     email =  db.Column(db.String(200), nullable=False)
+    db.session.commit()
     def __repr__(self):
         return self.id    
 
@@ -38,6 +40,7 @@ class Games(db.Model):
     team2 = db.Column(db.String(200), nullable=False)
     isDone =  db.Column(db.String(200), nullable=False)
     winner =  db.Column(db.String(200), nullable=False)
+    db.session.commit()
     def __repr__(self):
         return self.id
 
