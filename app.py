1308071291b238cb37a69ce5e398d2490a5a3ea8
Shreyas <schicker@purdue.edu>
2022-01-23 04:36:14 -0500

    added db functions after models

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import random
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///master.db'
 db = SQLAlchemy(app)
-db.create_all()
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,7 +16,6 @@ class Users(db.Model):
     password =  db.Column(db.String(200), nullable=False)
     confirm = db.Column(db.String(200), nullable=False)
     boilerBucks = db.Column(db.Integer, primary_key = False)
-    db.session.commit()
     def __repr__(self):
         return self.id
 
@@ -28,7 +26,6 @@ class Bets(db.Model):
     betAmount = db.Column(db.Integer, primary_key=False)
     payout =  db.Column(db.Integer, primary_key=False)
     email =  db.Column(db.String(200), nullable=False)
-    db.session.commit()
     def __repr__(self):
         return self.id    
 
@@ -40,9 +37,10 @@ class Games(db.Model):
     team2 = db.Column(db.String(200), nullable=False)
     isDone =  db.Column(db.String(200), nullable=False)
     winner =  db.Column(db.String(200), nullable=False)
-    db.session.commit()
     def __repr__(self):
         return self.id
+db.create_all()
+db.session.commit()
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
